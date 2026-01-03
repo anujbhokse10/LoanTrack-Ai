06a5ea4c498996904160d82d4a6766336aeace53
Anuj Bhokse <anujbhokse2210@gmail.com>
2026-01-03 07:55:38 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -11,12 +11,12 @@ function getFirebase() {
     if (typeof window !== 'undefined') {
         if (!getApps().length) {
             const firebaseConfig = {
-              apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "your-api-key",
-              authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "your-auth-domain",
-              projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "your-project-id",
-              storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "your-storage-bucket",
-              messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "your-messaging-sender-id",
-              appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "your-app-id",
+              apiKey: "AIzaSyA1nBKGDPXYDwOkT0KTehx4Vx2yoDX--08",
+              authDomain: "studio-6453477558-a93d9.firebaseapp.com",
+              projectId: "studio-6453477558-a93d9",
+              storageBucket: "studio-6453477558-a93d9.appspot.com",
+              messagingSenderId: "1007160010554",
+              appId: "1:1007160010554:web:86535337d62379da445203",
             };
             app = initializeApp(firebaseConfig);
             auth = getAuth(app);
