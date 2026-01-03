86e841a18d04c5069286b08479b034b6facf00d0
Anuj Bhokse <anujbhokse2210@gmail.com>
2026-01-03 07:48:35 +0000

    Try fixing this error: `Runtime FirebaseError: Firebase: Error (auth/inv

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,6 +2,9 @@
 import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
 import { getAuth, type Auth } from "firebase/auth";
 import { getFirestore, type Firestore } from "firebase/firestore";
+import { config } from 'dotenv';
+
+config();
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
